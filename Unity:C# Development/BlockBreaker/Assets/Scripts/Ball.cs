9985500ac803fc9bcf7875bbcdedbdd112ebf83a
JayTayah <jonathan.tayahh@gmail.com>
2020-08-21 20:54:27 -0700

    Added Unity Files

---
+++ Unity:C# Development/BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] float initialVelocityX = 2f;
+    [SerializeField] float initialVelocityY = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
+
+
+
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidBody;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            myRigidBody.velocity = new Vector2(initialVelocityX, initialVelocityY);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddleToBallVector + paddlePos;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 veloctiyTweek = new Vector2
+                                (Random.Range(0f, randomFactor),
+                                Random.Range(0f,randomFactor));
+        if (hasStarted)
+        {
+            AudioClip sound = ballSounds[Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(sound);
+            myRigidBody.velocity += veloctiyTweek;
+        }
+    }
+}
---
+++ Unity:C# Development/BlockBreaker/Assets/Scripts/Block.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] damageLevels;
+
+    Level level;
+
+    [SerializeField] int timesHit;
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            CheckHits();
+        }
+    }
+
+    private void CheckHits()
+    {
+        timesHit++;
+        int maxHits = damageLevels.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (damageLevels[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = damageLevels[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block Sprite is missing in Array" + gameObject.name);
+        }
+            
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        TriggerSparklesVFX();
+        level.BlockDestroyed();
+        FindObjectOfType<GameSession>().AddToScore();
+        
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 2f);
+    }
+}
